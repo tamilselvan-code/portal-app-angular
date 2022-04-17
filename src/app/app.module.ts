21667220b9a6d4fd0a3a38fa3f8774c263256fa2
Tamilselvan <tamilselvan.coder@gmail.com>
2022-04-17 16:52:54 +0530

    added components departments, examinations and results

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentsComponent } from './departments/departments.component';
+import { ExaminationsComponent } from './examinations/examinations.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentsComponent,
+    ExaminationsComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
